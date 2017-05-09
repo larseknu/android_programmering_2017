aec0d50f9d2d86ae38c743acd18fb8318c1ab187
Lars Emil <larsemil87@gmail.com>
2017-05-09 10:30:40 +0200

    Fixed the Material Design example

--- Forelesning06/PlayingWithMaterialDesign/app/src/main/java/no/hiof/larseknu/playingwithmaterialdesign/ColorActivity.java
+++ Forelesning06/PlayingWithMaterialDesign/app/src/main/java/no/hiof/larseknu/playingwithmaterialdesign/ColorActivity.java
@@ -1,10 +1,13 @@
 package no.hiof.larseknu.playingwithmaterialdesign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class ColorActivity extends AppCompatActivity {
+public class ColorActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +16,22 @@ public class ColorActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
 
-        setSupportActionBar(toolbar);
-
         toolbar.setTitle("Colors");
+
+        toolbar.inflateMenu(R.menu.main_menu);
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.animals:
+                Intent intent = new Intent(this, AnimalActivity.class);
+                startActivity(intent);
+                break;
+        }
+
+        return true;
     }
 }
